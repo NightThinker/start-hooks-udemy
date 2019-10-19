1bc2b9586863625f2e32c3ced0104fa53930515a
NightThinker <travel_special@hotmail.com>
2019-10-19 16:05:09 +0700

    Multiple States

--- src/components/Ingredients/IngredientForm/IngredientForm.js
+++ src/components/Ingredients/IngredientForm/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../../UI/Card/Card';
 import { IngredientFormStyle, FormControlStyle, IngredientFormActions } from './IngredientFormStyle';
 
 const IngredientForm = React.memo(props => {
-	const [ inputState, setInputState ] = useState({ title: '', amout: '' });
+	const [ title, setTitle ] = useState('');
+	const [ amount, setAmount ] = useState('');
 	const submitHandler = event => {
 		event.preventDefault();
 		// ...
@@ -16,20 +17,15 @@ const IngredientForm = React.memo(props => {
 				<form onSubmit={submitHandler}>
 					<FormControlStyle>
 						<label htmlFor='title'>Name</label>
-						<input
-							type='text'
-							id='title'
-							value={inputState.title}
-							onChange={event => setInputState({ ...inputState, title: event.target.value })}
-						/>
+						<input type='text' id='title' value={title} onChange={event => setTitle(event.target.value)} />
 					</FormControlStyle>
 					<FormControlStyle>
 						<label htmlFor='amount'>Amount</label>
 						<input
 							type='number'
 							id='amount'
-							value={inputState.amout}
-							onChange={event => setInputState({ ...inputState, amout: event.target.value })}
+							value={amount}
+							onChange={event => setAmount(event.target.value)}
 						/>
 					</FormControlStyle>
 					<IngredientFormActions>
