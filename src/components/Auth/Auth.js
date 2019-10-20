1548f63b707f24d70725d61c348596a83bf806f4
NightThinker <travel_special@hotmail.com>
2019-10-20 14:18:19 +0700

    auth useContext

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card/Card';
+import { AuthContext } from '../../context/auth-context';
 import { AuthStyle } from './AuthStyle';
 
 const Auth = props => {
-	const loginHandler = () => {};
+	const authContext = useContext(AuthContext);
+	const loginHandler = () => {
+		authContext.login();
+	};
 
 	return (
 		<AuthStyle>
